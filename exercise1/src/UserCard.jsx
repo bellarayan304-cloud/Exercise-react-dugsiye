4b5b2bba7167c38d26d1d91044c7afed9afc1d3a
bellarayan304-cloud <bellarayan304@gmail.com>
2026-08-27 06:58:00 +0300

    Frist  project

---
+++ exercise1/src/UserCard.jsx
@@ -0,0 +1,16 @@
+
+
+
+ export const UserCard =()=>{
+
+
+    return(
+        <div>
+            <h1>layla abdikadir abshir</h1>
+            <h1>laylaabdikadir@gmail.com</h1>
+
+        </div>
+
+    )
+}
+;
