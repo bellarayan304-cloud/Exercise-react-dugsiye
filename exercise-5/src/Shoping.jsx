903396af95150012f19f4f19eb24c662145d4e25
bellarayan304-cloud <bellarayan304@gmail.com>
2026-08-27 06:37:12 +0300

    Update project

--- exercise-5/src/Shoping.jsx
+++ exercise-5/src/Shoping.jsx
@@ -1,53 +1,102 @@
 
 import { useState } from "react"
-const shopping =()=>{
-
-
-
+const shopping = () => {
 
   const [shope, setShope] = useState([])
   const [Value, setValue] = useState("")
   const [price, setPrice] = useState("")
-const handling = ()=>{
+  const handling = () => {
 
-const newTodo   = {
-  id:crypto.randomUUID(),
-  text:Value,
-  price:price,
-  compelet:false
-  
+    if (Value.trim() !== "" && price.trim()) {
 
-  
-}
-// console.log(handling)
-   setShope([...shope,newTodo])
-// //    console.log(shope)
-setValue("");
-    setPrice("");
+      const newTodo = {
+        id: crypto.randomUUID(),
+        text: Value,
+        price: parseFloat(price),
 
-}
+        quantity: 1,
+        compelet: false
+      }
+      setShope([...shope, newTodo])
+      //    console.log(shope)
+      setValue("");
+      setPrice("");
 
-  return(
-    <>
-    <h2>shopping card</h2>
-    <input type="text" placeholder="prodect Name" onChange={(Event) => setValue(Event.target.value)}  value={Value} />
-    <input type="number" placeholder="price"onChange={(Event) => setPrice(Event.target.value)}  value={price} />
-    <button onClick={handling}>Add</button>
-    <ul>
-      {
-      
-        shope.map( shope =>(
-        <li>{shope.text}</li>,
-         <li key={shope.id} >
-             {shope.text}  ${shope.price}
-         </li>
-        
-        
-      
-      ))
     }
-    </ul>
-    </>
+
+
+  }
+  const removeShope = (id) => {
+    const updateShope = shope.filter(shope => shope.id !== id)
+    setShope(updateShope)
+
+  }
+  const increaseShope = (id) => {
+    const updateShope = shope.map(shope => (
+      shope.id === id ? { ...shope, quantity: shope.quantity + 1 } : shope
+
+    ))
+    setShope(updateShope)
+  }
+
+  const decreaseShopet = (id) => {
+    const updateShope = shope.map(shope => (
+      shope.id === id ? { ...shope, quantity: shope.quantity - 1 } : shope
+
+    ))
+    setShope(updateShope)
+  }
+  // console.log(handling)
+
+
+
+
+  return (
+    <div>
+      <h1>shopping card</h1>
+      <div>
+        <h2>Add prodect</h2>
+
+        <input type="text" placeholder="prodect Name" onChange={(Event) => setValue(Event.target.value)} value={Value} />
+        <input type="number" placeholder="price" onChange={(Event) => setPrice(Event.target.value)} value={price} />
+        <button onClick={handling}>Add</button>
+
+
+        {
+          shope.length > 0 ? (
+            <div>
+              <ul>
+                <h2>prodects in cart</h2>
+
+
+
+                {
+                  shope.map(shope => (
+                    <li key={shope.id}>
+                      <strong>{shope.text}</strong>
+                      <span> = ${shope.price.toFixed(2)}</span>
+
+                      <div>
+                        quantity:
+                        <button onClick={() => decreaseShopet(shope.id)}>-</button>
+                        {shope.quantity}
+                        <button onClick={() => increaseShope(shope.id)}>+</button>
+
+                      </div>
+                      <button onClick={() => removeShope(shope.id)}>Remove</button>
+                    </li>
+
+                  ))
+                }
+              </ul>
+            </div>
+          ) : <p>emty prodect</p>
+        }
+
+      </div>
+    </div>
   )
+
 }
+
 export default shopping;
