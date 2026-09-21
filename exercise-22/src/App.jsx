8c6416b59733cb5b2f830c9258667b3dfedc91dd
bellarayan304-cloud <bellarayan304@gmail.com>
2026-09-21 17:09:10 +0300

    Update exercise 22

--- exercise-22/src/App.jsx
+++ exercise-22/src/App.jsx
@@ -4,30 +4,34 @@ function App(){
   const [formData, setFormData] =useState({
     username:"",
     email:"",
-    password:""
+    password:"",
+    country:"",
+    agreement:false
   })
   const [isCheck, setIsCheck]=useState(false)
   const [select, setSelect]=useState("")
   const handleSubmit=(e)=>{
     e.preventDefault();
+      console.log(formData)
 
-    if(!isCheck){
-      alert("please the checked ")
-      return
-    }
-    if(select == ""){
-      alert("please choose select optional")
-      return
-    }
-     console.log(isCheck)
-     console.log(select)
+    // if(!isCheck){
+    //   alert("please the checked ")
+    //   return
+    // }
+    // if(select == ""){
+    //   alert("please choose select optional")
+    //   return
+    // }
+    //  console.log(isCheck)
+    //  console.log(select)
 
   }
   const handleChange=(e)=>{
     const {name,value}=e.target
-    console.log({name})
-    console.log({value})
+    // console.log({name})
+    // console.log({value})
     setFormData((preData)=>({...preData, [name]:value}))
+  
   }
   const handleCheck=(e)=>{
     setIsCheck(e.target.checked)
@@ -36,18 +40,21 @@ function App(){
 
   return(
     <div>
-      <form onChange={handleSubmit}>
-        <input type="text" name="username" placeholder="Enter The Name" value={formData.username} onChange={handleChange} />
-        <input type="text" name="email" placeholder="Enter The Email" value={formData.email} onChange={handleChange} />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="username" placeholder="Enter The Name" value={formData.username} 
+        onChange={handleChange} />
+
+        <input type="email" name="email" placeholder="Enter The Email" required value={formData.email} onChange={handleChange} />
+
         <input type="password" name="password" placeholder="Enter The password" value={formData.password} onChange={handleChange} />
+
         <label htmlFor="">checkbox</label>
-        <input type="checkbox" checked={isCheck} onChange={handleCheck} />
-        <select value={select} onChange={(e)=> (setSelect(e.target.value))}>
+        <input name="agreement" type="checkbox" checked={formData.agreement} onChange={handleChange} />
+
+        <select name="country" value={formData.country} onChange={handleChange}>
             <option value="">selected</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
+            <option value="somalia">somalia</option>
+            <option value="kenya">kenya</option>
 
         </select>
         <button type="submit">submit</button>
